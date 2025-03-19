fa0ffefb99efea59d55b899711e3fc515565efa3
Mahyar Kazazi <rihem1386@gmail.com>
2025-03-19 13:49:53 +0330

    Add Aura Widget Classes

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Mahyar Kazazi.
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
